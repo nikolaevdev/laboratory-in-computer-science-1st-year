9310d4010f944cf397fe7708c92844d96c954d42
Nikolaev Misha <mishanikolaevalmet@ya.ru>
2021-09-30 21:03:30 +0300

    Lab 3+

---
+++ Lab 3/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace Lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Решить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "B";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(172, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 125);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(172, 172);
+            this.textBox4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(202, 309);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(218, 348);
+            this.MinimumSize = new System.Drawing.Size(218, 348);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Lab 3/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            string tmp = textBox1.Text.Trim();
+            string outS = string.Empty;
+            bool zapyataya = true;
+            foreach (char ch in tmp)
+
+                if (Char.IsDigit(ch) | Char.IsLetter(ch) || (ch == ',' && zapyataya))
+                {
+                    outS += ch;
+                    if (ch == ',')
+                        zapyataya = false;
+                }
+
+            textBox1.Text = outS;
+            textBox1.SelectionStart = outS.Length;
+
+            if (str.Contains("."))
+            {
+                string s = str.Replace(".", ",");
+                textBox1.Clear();
+                textBox1.AppendText(str.Replace(".", ","));
+
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            string str = textBox2.Text;
+            string tmp = textBox2.Text.Trim();
+            string outS = string.Empty;
+            bool zapyataya = true;
+            foreach (char ch in tmp)
+
+                if (Char.IsDigit(ch) | Char.IsLetter(ch) || (ch == ',' && zapyataya))
+                {
+                    outS += ch;
+                    if (ch == ',')
+                        zapyataya = false;
+                }
+
+            textBox2.Text = outS;
+            textBox2.SelectionStart = outS.Length;
+
+            if (str.Contains("."))
+            {
+                string s = str.Replace(".", ",");
+                textBox2.Clear();
+                textBox2.AppendText(str.Replace(".", ","));
+
+            }
+        }
+
+        private double f(double a)
+        {
+            return Math.Pow(a, 2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double x = 0;
+            double b = 0;
+            double g = 0;
+
+            if (textBox1.Text.Length == 0)
+            {
+                MessageBox.Show("Введите x");
+            }
+            else if (textBox2.Text.Length == 0)
+            {
+                MessageBox.Show("Введите y");
+            } else {
+
+                x = double.Parse(textBox1.Text);
+                b = double.Parse(textBox2.Text);
+
+                if((0.5 < x*b) && (x*b < 10))
+                {
+                    g = Math.Exp(f(x) - Math.Abs(b));
+
+                } else if ((0.1 < x*b) && (x*b < 0.5))
+                {
+                    g = Math.Sqrt(Math.Abs(f(x) + b));
+                } else
+                {
+                    g = 2 * Math.Pow(f(x), 2);
+                }
+
+                textBox4.Text = "Результаты работы программы " + Environment.NewLine;
+                textBox4.Text += "При X =" + textBox1.Text + Environment.NewLine;
+                textBox4.Text += "При Y =" + textBox2.Text + Environment.NewLine;
+                textBox4.Text += "g =" + g.ToString();
+
+            }
+
+        }
+    }
+}
